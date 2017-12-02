86d573d64da21b1d8bdd8a18ae97c60b7feb5fbc
m1lwoj <milwojarski@gmail.com>
2017-12-02 22:13:23 +0100

    Minor fix

--- src/index.js
+++ src/index.js
@@ -8,19 +8,15 @@ import {Provider} from 'react-redux';
 import {Router, browserHistory } from 'react-router';
 import routes from './routes.js';
 import './styles/styles.css';
-import {loadCourses} from './actions/courseActions';
-import {loadAuthors} from './actions/authorActions';
 import {loadRoutes} from './actions/routeActions';
 import {loadPoints} from './actions/routeActions';
 import {loadToken} from './actions/userActions';
 import {loadUserInfo} from './actions/userActions';
 import {} from './actions/authorActions';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-        injectTapEventPlugin();
+injectTapEventPlugin();
 
 const store = configureStore();
-store.dispatch(loadCourses());
-store.dispatch(loadAuthors());
 store.dispatch(loadToken());
 store.dispatch(loadUserInfo());
 
